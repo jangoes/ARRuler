9ce52d5cc42e119e7124c335ffa14f5baf675e01
John Ellie Go <ellie.go12@gmail.com>
2021-03-30 14:08:07 +0800

    ARRuler

--- ARRuler/ViewController.swift
+++ ARRuler/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var dotaNodes = [SCNNode]()
+    
+    var textNode = SCNNode()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,6 +25,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
         // Create a new scene
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
@@ -45,30 +50,75 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if dotaNodes.count >= 2 {
+            dotaNodes.forEach { (node) in
+                node.removeFromParentNode()
+            }
+            
+            dotaNodes.removeAll()
+        }
         
+        if let touchLocation = touches.first?.location(in: sceneView) {
+            
+            guard let query = sceneView.raycastQuery(from: touchLocation, allowing: .estimatedPlane, alignment: .any) else {
+               return
+            }
+            
+            let results = sceneView.session.raycast(query)
+            
+            if let hitResult = results.first {
+                addDot(at: hitResult)
+            }
+        }
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    func addDot(at hitResult: ARRaycastResult) {
+        let dotGeometry = SCNSphere(radius: 0.005)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
         
+        dotGeometry.materials = [material]
+        
+        let dotNode = SCNNode(geometry: dotGeometry)
+        let columns = hitResult.worldTransform.columns.3
+        
+        dotNode.position = SCNVector3(x: columns.x, y: columns.y, z: columns.z)
+        
+        dotaNodes.append(dotNode)
+        
+        sceneView.scene.rootNode.addChildNode(dotNode)
+        
+        if dotaNodes.count >= 2 {
+            calculate()
+        }
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
+    func calculate() {
+        let start = dotaNodes[0]
+        let end = dotaNodes[1]
+        
+        let x = end.position.x - start.position.x
+        let y = end.position.y - start.position.y
+        let z = end.position.z - start.position.z
+        
+        let distance = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2))
+        
+        updateText(text: "\(abs(distance))", at: end.position)
+    }
+    
+    func updateText(text: String, at position: SCNVector3) {
+        textNode.removeFromParentNode()
+        
+        let textGeometry = SCNText(string: text, extrusionDepth: 1)
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.red
+        
+        textNode = SCNNode(geometry: textGeometry)
+        textNode.position = SCNVector3(position.x, position.y + 0.1, position.z)
+        textNode.scale = SCNVector3(0.01, 0.01, 0.01)
+        
+        sceneView.scene.rootNode.addChildNode(textNode)
         
     }
 }
